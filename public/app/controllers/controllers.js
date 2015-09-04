f2b729a9baf57ad5ef9e7166db136f43049442a8
JoshSpears3 <josh.spears@sq1.com>
2015-09-03 19:27:21 -0500

    Created a slider! New directive, and controller. Sass updates.

--- public/app/controllers/controllers.js
+++ public/app/controllers/controllers.js
@@ -6,9 +6,23 @@ myApp.controller('ContactCtrl', ['$scope', function($scope) {
   $scope.greeting = 'Contact Page!';
 }]);
 
-myApp.controller('githubCtrl', [ 'githubApi', '$q', '$scope',
-    function(githubApi, $q, $scope){
-        $scope.getInfo = function(name){
+//Wrap controller in a IIFE closure to avoid global conflicts with variables
+(function() {
+    'use strict';
+    myApp.controller('githubCtrl', [ 'githubApi', '$q', '$scope',
+        function(githubApi, $q, $scope){
+            $scope.getInfo = function(name){
+                githubApi.accountInformation($scope.username)
+                .then( function (result) {
+                        // Wrap data returned in a object name data
+                        $scope.data = result.data;
+                    }, function (error) {
+                        // Handle errors here
+                        console.log(error.statusText);
+                    }
+                )
+                .getInfo(name);
+            }
             githubApi.accountInformation($scope.username)
             .then( function (result) {
                     // Wrap data returned in a object name data
@@ -18,19 +32,9 @@ myApp.controller('githubCtrl', [ 'githubApi', '$q', '$scope',
                     console.log(error.statusText);
                 }
             )
-            .getInfo(name);
         }
-        githubApi.accountInformation($scope.username)
-        .then( function (result) {
-                // Wrap data returned in a object name data
-                $scope.data = result.data;
-            }, function (error) {
-                // Handle errors here
-                console.log(error.statusText);
-            }
-        )
-    }
-]);
+    ]);
+})();
 
 myApp.controller('HomeCtrl', ['$scope', function($scope) {
   $scope.greeting = 'Home Page!';
@@ -45,6 +49,45 @@ myApp.controller('repeatCtrl', ['$scope',
         ]
     }
 ]);
+
+myApp.controller('sliderCtrl', ['$scope', function($scope){
+    // Create an array of slide images
+    $scope.sliderImages = [
+        { image: '../../public/assets/img/joker2.jpg', description: 'Image 1' },
+        { image: '../../public/assets/img/batman1.jpg', description: 'Image 2' },
+        { image: '../../public/assets/img/joker1.jpg', description: 'Image 3' }
+    ];
+
+    // Initially the index is at the first image
+    $scope.currentIndex = 0;
+
+    //Display next image in array
+    $scope.next = function(){
+        if ($scope.currentIndex < $scope.sliderImages.length - 1) {
+            $scope.currentIndex++
+        } else {
+            $scope.currentIndex = 0;
+        }
+    };
+
+    //Display prev image in array
+    $scope.prev = function() {
+        if ($scope.currentIndex > 0) {
+            $scope.currentIndex--
+        } else {
+            $scope.currentIndex = $scope.sliderImages.length - 1;
+        }
+    };
+
+    $scope.$watch('currentIndex', function() {
+        $scope.sliderImages.forEach(function(image) {
+            // make every image invisible
+            image.visible = false;
+        });
+        // make the current image visible
+        $scope.sliderImages[$scope.currentIndex].visible = true;
+    });
+}]);
 
 myApp.controller('WorkCtrl', ['$scope', function($scope) {
   $scope.greeting = 'Work Page!';
